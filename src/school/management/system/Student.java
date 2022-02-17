ea4fcd79dcf32cc3ee461b291bbcea89c5463e52
Emre Can Satık <emrecanstk@gmail.com>
2022-02-18 02:06:14 +0300

    add a constructer with three params

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -1,11 +1,29 @@
 package school.management.system;
 
 public class Student {
-
     private int id;
     private String name;
     private int grade;
     private int feesPaid;
     private int feesTotal;
+
+    /**
+     * To create a new student by initializing.
+     * Fees for every students is 30,000TL;
+     * Fees paid initially is 0 TL;
+     * @param id for the students: unique.
+     * @param name of the students.
+     * @param grade of the students.
+     */
+
+    public Student(int id, String name, int grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+    }
+
+
 
 }
