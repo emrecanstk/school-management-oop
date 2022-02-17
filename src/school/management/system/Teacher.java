fe21ce33371ae8cc182d889cc8d14d5e2f5643bd
Emre Can Satık <emrecanstk@gmail.com>
2022-02-18 02:21:26 +0300

    add a constucter for Teacher class

--- src/school/management/system/Teacher.java
+++ src/school/management/system/Teacher.java
@@ -4,6 +4,18 @@ public class Teacher {
     private int id;
     private String name;
     private int salary;
+
+    /**
+     * Creates a new Teacher object.
+     * @param id for the teacher: unique.
+     * @param name of the teacher.
+     * @param salary of the teacher.
+     */
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
     
 
 }
