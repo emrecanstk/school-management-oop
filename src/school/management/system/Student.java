3c776015c33991a3f9a37e06e886d8e3a97d51eb
Emre Can Satık <emrecanstk@gmail.com>
2022-02-18 01:54:50 +0300

    add attributes for Student class

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -1,4 +1,11 @@
 package school.management.system;
 
 public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
 }
