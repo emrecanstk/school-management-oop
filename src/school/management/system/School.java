faca2c98d7f964233f5348e530dbb79d7d81d024
Emre Can Satık <emrecanstk@gmail.com>
2022-02-18 02:42:05 +0300

    add a constructer for School class

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -8,5 +8,18 @@ public class School {
     private int totalMoneyEarned;
     private int totalMoneySpend;
 
+    /**
+     * New school object created.
+     *
+     * @param teachers list of teachers in the school.
+     * @param students list of students in the school.
+     */
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpend = 0;
+    }
+
     
 }
