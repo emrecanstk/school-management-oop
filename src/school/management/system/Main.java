844dbc88260cd611d76c3cf34c00d89a674c1a21
Emre Can Satık <emrecanstk@gmail.com>
2022-02-18 03:02:55 +0300

    create objects of teacher

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -1,6 +1,15 @@
 package school.management.system;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Teacher aybuke = new Teacher(1,"Şenay Aybüke Yalçın",1300);
+        Teacher necmettin = new Teacher(2,"Necmettin Yılmaz",1100);
+        Teacher nese = new Teacher(3,"Neşe Alten",1200);
+        Teacher huseyin = new Teacher(4,"Hüseyin Aydemir",1150);
+        Teacher mustafa = new Teacher(5,"Mustafa Çağlar",1400);
+        Teacher dilay = new Teacher(6,"Dilay Kermen",1300);
+        
+    }
 
 }
